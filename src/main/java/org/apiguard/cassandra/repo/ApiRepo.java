f1d5a0129bff2e649b6309dbf5df0e75d03c3696
Stanley Lam <slam@slam014575.starbucks.net>
2017-04-27 23:09:15 -0700

    added regex support in incoming url

--- src/main/java/org/apiguard/cassandra/repo/ApiRepo.java
+++ src/main/java/org/apiguard/cassandra/repo/ApiRepo.java
@@ -1,11 +1,19 @@
 package org.apiguard.cassandra.repo;
 
 import org.apiguard.cassandra.entity.ApiEntity;
+import org.springframework.data.cassandra.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
-public interface ApiRepo extends CrudRepository<ApiEntity, String> {
-//
-//	@Query("select * from api where name = ?0")
-//	Iterable<ApiEntity> findByName(String name);
+import java.util.List;
 
+public interface ApiRepo extends CrudRepository<ApiEntity, String> {
+
+    //    @Query("SELECT * FROM associations WHERE lucene = '{query : {type : \"match\",field : \"physicianid\",value :?0}}'")
+    //
+    //	@Query("select * from api where name = ?0")
+    //	Iterable<ApiEntity> findByName(String name);
+
+
+    @Query("select * from api where requri > ?0 ALLOW FILTERING")
+    List<ApiEntity> findByReqUriRegex(String reqUri);
 }
